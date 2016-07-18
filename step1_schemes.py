956070e31cd8d211ce2e762e1f44b137be44125b
Clay Budin <clay.budin@gmail.com>
2016-07-18 14:07:58 -0400

    Added Lax-Friedrich and Law-Wendroff schemes

--- step1_schemes.py
+++ step1_schemes.py
@@ -11,6 +11,7 @@ linear because c is constant
 Schemes:
 Upwind - Forward diff in time, Backward diff in space
 Leapfrog - Centered diff in time, u(n+1,i) = u(n-1,i) - c*dt/dx * (u(n,i+1)-u(n,i-1)) - centered diff using point from 2 time steps back
+Lax-Friedrichs - FD in time, u(n+1,i) = 1/2*(u(n,i+1)+u(n,i-1)) - c*dt/(2*dx) * (u(n,i+1)-u(n,i-1)) - centered diff using avg for u(n,i)
 
 Domain: [0,2]
 Range: [0,1]
@@ -35,6 +36,12 @@ fig, ax = plt.subplots()
 ax.grid()
 line_uw, = ax.plot([], [], "b", lw=1)
 line_lf, = ax.plot([], [], "r", lw=1)
+line_lxfr, = ax.plot([], [], "g", lw=1)
+line_lw, = ax.plot([], [], "c", lw=1)
+
+ax.set_xlim(-.1, 2.1)
+ax.set_ylim(-.1, 1.4)
+
 
 # simulation constants
 nx = 201
@@ -47,6 +54,8 @@ dx = 2.0 / (nx-1.0)
 xdata = []
 yduw = []				# upwind scheme - backwards difference
 ydlf, yplf = [], []		# leapfrog scheme - save previous data since we need u(t-2)
+ydlxfr = []
+ydlw = []
 
 
 # global var - current time step
@@ -57,12 +66,13 @@ def init ():
 	if ct == 0: print "init() dx = " + str(dx) + " dt = " + str(dt) + " c*dt/dx = " + str(c*dt/dx)
 	ct = 0
 
-	ax.set_xlim(-.1, 2.1)
-	ax.set_ylim(-.1, 1.1)
 	del xdata[:]
 	del yduw[:]
 	del ydlf[:]
 	del yplf[:]
+	del ydlxfr[:]
+	del ydlw[:]
+
 	for i in range(nx):
 		x = i*dx
 		xdata.append(x)
@@ -74,8 +84,15 @@ def init ():
 		yduw.append(y)
 		ydlf.append(y)
 		yplf.append(y)
+		ydlxfr.append(y)
+		ydlw.append(y)
+
 	line_uw.set_data(xdata, yduw)
 	line_lf.set_data(xdata, ydlf)
+	line_lxfr.set_data(xdata, ydlxfr)
+	line_lw.set_data(xdata, ydlw)
+
+
 
 
 def data_gen ():
@@ -84,7 +101,7 @@ def data_gen ():
 		#print "data_gen() ct =", ct
 		ct += 1
 
-		# upwind scheme (backwards diff)
+		# Upwind scheme (backwards diff)
 		# copy contents of yduw to ypuw
 		ypuw = []
 		ypuw[:] = yduw[:]
@@ -96,14 +113,14 @@ def data_gen ():
 
 		# leapfrog scheme
 		# stable for sigma <= 1.0
-		# better than upwind on leading edge, but trailing edge has ringing
-		# http://www.thevisualroom.com/heavy_side_and_sinusoidal_input.html#leapfrog-with
+		# better than upwind on leading edge, but trailing edge has oscillations
+		# see: http://www.thevisualroom.com/heavy_side_and_sinusoidal_input.html#leapfrog-with
 		ypplf = []
 		ypplf[:] = yplf[:]
 		yplf[:] = ydlf[:]
 		if ct == 1:
 			# need a startup scheme - upwind
-			print "Startup"
+			#print "Startup"
 			for i in range(1,nx-1):
 				ydlf[i] = yplf[i] - (c*dt/dx)*(yplf[i]-yplf[i-1])
 		else:
@@ -112,9 +129,22 @@ def data_gen ():
 				ydlf[i] = ypplf[i] - (c*dt/dx)*(yplf[i+1]-yplf[i-1])
 
 
+		# Lax-Friedrichs
+		# seems like worse numerical diffusion than with Upwind
+		yplxfr = []
+		yplxfr[:] = ydlxfr[:]
+		for i in range(1,nx-1):
+			ydlxfr[i] = .5*(yplxfr[i+1]+yplxfr[i-1]) - (c*dt/(2*dx))*(yplxfr[i+1]-yplxfr[i-1])
+
+		# Lax-Wendroff
+		# improved on leading edge like leapfrog, oscillations on trailing edge but better than leapfrog
+		yplw = []
+		yplw[:] = ydlw[:]
+		for i in range(1,nx-1):
+			ydlw[i] = yplw[i] - (c*dt/(2*dx))*(yplw[i+1]-yplw[i-1]) + (c*dt/dx)*(c*dt/dx)*.5*(yplw[i+1]-2*yplw[i]+yplw[i-1])
 
 
-		# don't need to enfore BC since leftmost element of ydata arrays not touched but iteration
+		# don't need to enfore BC since leftmost element of ydata arrays not touched by iterations
 
 		yield
 
@@ -123,6 +153,8 @@ def run (data):
 	#print "run()"
 	line_uw.set_ydata(yduw)
 	line_lf.set_ydata(ydlf)
+	line_lxfr.set_ydata(ydlxfr)
+	line_lw.set_ydata(ydlw)
 
 
 
